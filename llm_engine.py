86ada2f82b8b1fbd7b14a3543a7cb9db432b40b0
Kusum Vishwakarma <kusumv014@gmail.com>
2026-04-23 21:08:26 +0400

    added gemini integration

--- llm_engine.py
+++ llm_engine.py
@@ -1,15 +1,25 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.getenv("AIzaSyAASdFKKusjM-E0qtF8MNYUfelklM_eGV0"))
+
+model = genai.GenerativeModel("gemini-pro")
+
 def analyze_resume(text):
-    return f"""
-Skills detected:
-- Python
-- SQL
-- Excel
+    try:
+        response = model.generate_content(
+            f"""
+            Analyze this resume and provide:
+            1. Key skills
+            2. Improvements
+            3. Rating out of 10
 
-Suggestions:
-- Add more projects
-- Improve formatting
+            Resume:
+            {text}
+            """
+        )
 
-Rating: 7/10
-"""
+        return response.text
 
-
+    except Exception as e:
+        return f"Error: {str(e)}"
